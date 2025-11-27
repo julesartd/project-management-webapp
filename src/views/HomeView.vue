562e2f9a2ddee0207c74901cf784bfdf08b29884
Kilian Levasseur <106158626+KilianL05@users.noreply.github.com>
2025-11-27 12:15:45 +0100

    fix stats

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,14 +1,9 @@
 <script setup>
 import AppLayout from '@/components/AppLayout.vue'
 import ProjectDashboard from '@/components/projects/ProjectDashboard.vue'
-import { generateMockData } from '@/utils/mockData'
 </script>
-
 <template>
   <AppLayout>
-    <div class="mb-4 flex justify-end">
-      <a-button @click="generateMockData" type="dashed" size="small">Générer Données de Test</a-button>
-    </div>
     <ProjectDashboard />
   </AppLayout>
 </template>
